e8220267123e48b2cd79a9839c3a6398c2e4c707
bluphy <bluphyxw@gmail.com>
2018-05-29 18:02:25 +0800

    fix some bugs

--- xTGWSimulator.py
+++ xTGWSimulator.py
@@ -17,17 +17,40 @@ HOST = '127.0.0.1'
 PORT = 9201             # The same port as used by the server
 
 PLAYBACK_FILE_PATH = 'D:\\bluphy\\Desktop\\bw_gb32960_test.log'
+TIMER_MSG_UPLOAD_PERIOD = 1
+VIN = 'LXVJ2GFC2GA030003'
+
+gInterrupt_flagstr = ''
 
 def responseCtrl_C(func):
+    global gInterrupt_flagstr
     def wrapper(*args, **kw):
 
         try:
             func(*args, **kw)
         except KeyboardInterrupt:
+            print('Interrupt by Ctrl-C')
+            gInterrupt_flagstr = 'Ctrl-C'
             return None
         
     return wrapper
 
+def sendMsg(socketinstance,msg):
+    try:
+        socketinstance.sendall(msg)
+    except:
+        print('[Warning]:Tx Msg fail:data-->',msg.hex())
+        return None
+    return 0
+    
+def cmd_login(socketinstance):
+    return sendMsg(socketinstance,b'##\x04\xFELXVJ2GFC2GA030003\x01\x00\x08\x11\x11\x11\x11\x11\x11\x33\x33\x33')
+    
+
+def cmd_logout(socketinstance):
+    return sendMsg(socketinstance,b'##\x04\xFELXVJ2GFC2GA030003\x01\x00\x08\x11\x11\x11\x11\x11\x11\x33\x33\x33')
+
+    
 def createOTAmsgfromGBlog(logline):
     msg = None
     if len(logline)>63:
@@ -37,53 +60,67 @@ def createOTAmsgfromGBlog(logline):
 
 def playback(playback_file_descriptor):
     for line in playback_file_descriptor:
-        msg = createOTAmsgfromGBlog(line)
+        msg = createOTAmsgfromGBlog(line)        
         yield msg
 
 @responseCtrl_C
 def cmd_pb(socketinstance):
+    global gInterrupt_flagstr
     with open(PLAYBACK_FILE_PATH) as f:
         for msg in playback(f):
             if msg:
-                socketinstance.sendall(msg)
-                #time.sleep(1)
-       
+                sendMsg(socketinstance,msg)                  
+                time.sleep(TIMER_MSG_UPLOAD_PERIOD)
+            
 def main():
+    global gInterrupt_flagstr
     while True:
-        cmd1=input('Please enter cmd:\n\tnew: create new connection\n\tquit: quit the program\n?>')
-        if cmd1 == 'new':
+        cmd1=input('Please enter cmd:\n\tn: create new connection\n\tquit: quit the program\n?>')
+        if cmd1 == 'n':
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect((HOST, PORT))
-                clientinfo = s.getsockname() 
-                while True:
-                    print('client:',clientinfo)
-                    cmd2=input('Please enter cmd:\n\tlogin: send login msg\n\tq: quit the program\n?>')
-                    if 'q' == cmd2:
-                        s.close()
-                        break
-                    elif 'login' == cmd2:
-                        s.sendall(b'##\x04\xFELXVJ2GFC2GA030003\x01\x08\x11\x11\x11\x11\x11\x11\x33\x33\x33')
-                        
-                    elif 'logout' == cmd2:
-                        pass
-                        
-                    elif 'rt' == cmd2: #realtime
-                        pass
-                        
-                    elif 'bf' == cmd2: #bufa
-                        pass
-                        
-                    elif 'bj' == cmd2: 
-                        pass
-                        
-                    elif 'pb' == cmd2: 
-                        cmd_pb(s)
-
-                    elif 'hb' == cmd2: #heartbeat
-                        pass         
+                connected = False
+                try:
+                    s.connect((HOST, PORT))
+                    connected =True
+                except:
+                    print('Connecting failed, retry after 10 seconds')
+                    time.sleep(10)
+                if connected:   
+                    clientinfo = s.getsockname() 
+                    while True:
+                        if len(gInterrupt_flagstr)>0:                            
+                            print('Exit cmd2 loop because of ',gInterrupt_flagstr)
+                            gInterrupt_flag = ''
                             
-                    else:
-                        s.sendall((cmd2+'\n').encode('utf8'))
+                            break
+                        print('client:',clientinfo)
+                        cmd2=input('Please enter cmd:\n\tlogin: send login msg\n\tq: quit the program\n?>')
+                        if 'q' == cmd2:
+                            s.close()
+                            break
+                        elif 'login' == cmd2:
+                            cmd_login(s)
+                            
+                        elif 'logout' == cmd2:
+                            cmd_logout(s)
+                            
+                        elif 'rt' == cmd2: #realtime
+                            pass
+                            
+                        elif 'bf' == cmd2: #bufa
+                            pass
+                            
+                        elif 'bj' == cmd2: 
+                            pass
+                            
+                        elif 'pb' == cmd2:
+                            cmd_pb(s)
+
+                        elif 'hb' == cmd2: #heartbeat
+                            pass         
+                                
+                        else:
+                            s.sendall((cmd2+'\n').encode('utf8'))
         elif cmd1 == 'quit':
             break
         #   
